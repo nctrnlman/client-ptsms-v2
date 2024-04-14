3ad143f1df2213798c09f51762c84d07bee8d7e5
Rhazes Devino <rhazesd@gmail.com>
2024-04-14 18:24:48 +0800

    feat(transaction) : feat transaction out

--- src/components/cards/DashboardCard.jsx
+++ src/components/cards/DashboardCard.jsx
@@ -1,9 +1,9 @@
 export default function DashboardCard({ title, description, icon }) {
   const Icon = icon;
   return (
-    <div className="max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-lg transition duration-300 hover:cursor-pointer">
+    <div className="max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 cursor-pointer transition duration-300 hover:scale-105 hover:shadow-lg">
       <div className="flex justify-center items-center gap-2 m-2">
-        <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
+        <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700 ">
           <Icon className="w-10 h-10  text-brand-500 dark:text-gray-400" />
         </div>
         <div className="">
