a2b78b1f32fe5af95c8ae22ee73bd8353c9b75da
oledunwahid <makkawirelang@gmai.com>
2024-04-16 11:53:57 +0700

    change color icon

--- src/components/cards/DashboardCard.jsx
+++ src/components/cards/DashboardCard.jsx
@@ -4,7 +4,7 @@ export default function DashboardCard({ title, description, icon }) {
     <div className="max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 cursor-pointer transition duration-300 hover:scale-105 hover:shadow-lg">
       <div className="flex justify-center items-center gap-2 m-2">
         <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700 ">
-          <Icon className="w-10 h-10  text-brand-500 dark:text-gray-400" />
+          <Icon className="w-10 h-10  text-teal-500 dark:text-gray-400" />
         </div>
         <div className="">
           <h5 className="font-dm text-sm font-medium text-gray-600">{title}</h5>
